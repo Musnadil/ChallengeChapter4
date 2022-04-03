02352e3abc2e4109a5cdf87e2d9a0b8032361cf1
Musnadil <firdaus.musnadil@gmail.com>
2022-04-03 21:52:07 +0700

    adding login feature

--- app/src/main/java/com/musnadil/challengechapter4/fragment/LoginFragment.kt
+++ app/src/main/java/com/musnadil/challengechapter4/fragment/LoginFragment.kt
@@ -1,16 +1,29 @@
 package com.musnadil.challengechapter4.fragment
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.musnadil.challengechapter4.LoginActivity
+import com.musnadil.challengechapter4.MainActivity
 import com.musnadil.challengechapter4.R
+import com.musnadil.challengechapter4.StoreDatabase
 import com.musnadil.challengechapter4.databinding.FragmentLoginBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 
 class LoginFragment : Fragment() {
+    var mDb: StoreDatabase? = null
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
@@ -23,14 +36,60 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mDb = StoreDatabase.getInstance(requireContext())
+
         binding.btnDaftar.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
         val username = arguments?.getString(RegisterFragment.USERNAME)
-        if (username.isNullOrEmpty()){
+        if (username.isNullOrEmpty()) {
             binding.etUsername.hint = ""
-        }else {
+        } else {
             binding.etUsername.setText(username)
+        }
+
+        binding.btnMasuk.setOnClickListener {
+
+            if (binding.etUsername.text.isNullOrEmpty()) {
+                binding.wrapUsername.error = "Kamu harus masukan username"
+            } else if (binding.etPassword.text.isNullOrEmpty()) {
+                binding.wrapPassword.error = "Kamu harus masukan password"
+            } else {
+                GlobalScope.async {
+                    val result = mDb?.storeDao()?.userCheck(
+                        binding.etUsername.text.toString(),
+                        binding.etPassword.text.toString()
+                    )
+                    runBlocking(Dispatchers.Main) {
+                        if (result == false) {
+                            closeKeyboard()
+                            val snackbar = Snackbar.make(it,"Gagal masuk mungkin anda salah memasukan email atau password",Snackbar.LENGTH_INDEFINITE)
+                            snackbar.setAction("Oke") {
+                                snackbar.dismiss()
+                                binding.etUsername.requestFocus()
+                                binding.etUsername.setText("")
+                                binding.etPassword.setText("")
+                            }
+                            snackbar.show()
+                        } else {
+                            Toast.makeText(activity, "Selamat datang ${binding.etUsername.text.toString()}", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(activity,MainActivity::class.java))
+                            onDestroy()
+                            activity?.finish()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun closeKeyboard() {
+        val activity = activity as LoginActivity
+
+        val view = activity.currentFocus
+        if (view != null) {
+            val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view!!.getWindowToken(), 0)
         }
     }
 
